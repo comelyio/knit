720a4f8225e27e4819e87354bbb1ba5f3540b036
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-13 21:28:24 +0500

    Caching

--- src/Caching.php
+++ src/Caching.php
@@ -14,11 +14,79 @@ declare(strict_types=1);
 
 namespace Comely\Knit;
 
+use Comely\Kernel\Toolkit\Number;
+use Comely\Knit\Exception\CachingException;
+
 /**
  * Class Caching
  * @package Comely\Knit
  */
 class Caching
 {
+    public const NONE = 1000;
+    public const NORMAL = 1001;
+    public const AGGRESSIVE = 1002;
 
+    /** @var int */
+    private $type;
+    /** @var int */
+    private $ttl;
+    /** @var null|string */
+    private $sessionId;
+
+    /**
+     * Caching constructor.
+     */
+    public function __construct()
+    {
+        $this->disable();
+        $this->ttl = 0;
+    }
+
+    /**
+     * @param int $seconds
+     * @return Caching
+     * @throws CachingException
+     */
+    public function ttl(int $seconds): self
+    {
+        if (!Number::Range($seconds, 0, PHP_INT_MAX)) {
+            throw new CachingException('Invalid caching TTL value');
+        }
+
+        $this->ttl = $seconds;
+        return $this;
+    }
+
+    /**
+     * @return Caching
+     */
+    public function disable(): self
+    {
+        $this->type = self::NONE;
+        $this->sessionId = null;
+        return $this;
+    }
+
+    /**
+     * @param string $sessionId
+     * @return $this
+     */
+    public function aggressive(string $sessionId)
+    {
+        $this->disable();
+        $this->type = self::AGGRESSIVE;
+        $this->sessionId = $sessionId;
+        return $this;
+    }
+
+    /**
+     * @return Caching
+     */
+    public function enable(): self
+    {
+        $this->disable();
+        $this->type = self::NONE;
+        return $this;
+    }
 }
