6791edb1d636ef95ba741f338fc4a0d1e9259ef1
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-14 05:54:29 +0500

    Parse exception

---
+++ src/Exception/ParseException.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of Knit package.
+ * https://github.com/comelyio/knit
+ *
+ *  Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/knit/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Knit\Exception;
+
+/**
+ * Class ParseException
+ * @package Comely\Knit\Exception
+ */
+class ParseException extends CompilerException
+{
+    /** @var int */
+    private $line;
+    /** @var null|string */
+    private $token;
+
+    /**
+     * ParseException constructor.
+     * @param string $message
+     * @param int $line
+     * @param string|null $token
+     */
+    public function __construct(string $message = "", int $line = 0, ?string $token = null)
+    {
+        $this->line = $line;
+        if ($token) {
+            $this->token = substr($token, 0, 16) . "...";
+        }
+
+        parent::__construct($message);
+    }
+
+    /**
+     * @return int
+     */
+    public function line(): int
+    {
+        return $this->line;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function token(): ?string
+    {
+        return $this->token;
+    }
+}
